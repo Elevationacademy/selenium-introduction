bff45c7e6091cea6afbcfb58454c976f2c2a5308
ronen-e <alter.ronen@gmail.com>
2017-04-06 14:05:53 +0300

    Add synchronous code

---
+++ selenium-sync.js
@@ -0,0 +1,12 @@
+const webdriver = require('selenium-webdriver')
+const { By, until, Builder } = webdriver;
+
+const driver = new Builder()
+    .forBrowser('chrome')
+    .build();
+
+driver.get('http://www.google.com/ncr');
+driver.findElement(By.name('q')).sendKeys('webdriver');
+driver.findElement(By.name('btnG')).click();
+driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+driver.quit();
