40dcccd3660f6363d8ec9fab01bf7fd4d011ace5
ronen-e <alter.ronen@gmail.com>
2017-04-06 11:56:15 +0300

    Add page behavior

--- public/bundle.js
+++ public/bundle.js
@@ -7,15 +7,27 @@ var images = [
   'http://www.top13.net/wp-content/uploads/2015/10/perfectly-timed-funny-cat-pictures-5.jpg',
   'https://s-media-cache-ak0.pinimg.com/736x/5e/43/55/5e4355de4f1aba37be8a9feaf1ba97c7.jpg'
 ];
+var I;
 
 function addCatToDOM() {
   if (catIndex < images.length) {
       var img = new Image();
       img.src = images[catIndex];
       img.id = 'cat_' + catIndex;
+      img.onclick = function() {
+        setTimeout(setImage.bind(null, this.src), 1000);
+      };
       document.body.append(img);
       catIndex++;
   }
 }
 
-setInterval(addCatToDOM, 1000);
+function setImage(src) {
+  clearInterval(I);
+  var body = document.body;
+  body.style.backgroundImage = 'url(' + src + ')';
+  body.innerHTML = '<h1 id="success"></h1>'
+}
+
+
+I = setInterval(addCatToDOM, 500);
