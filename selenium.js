34f7db458f4c8a1a51c005f09648a0872a7110db
ronen-e <alter.ronen@gmail.com>
2017-04-13 10:02:22 +0300

    Add asynchronous example code

--- selenium.js
+++ selenium.js
@@ -1,5 +1,3 @@
-// https://www.npmjs.com/package/selenium-webdriver
-
 const webdriver = require('selenium-webdriver')
 const { By, until, Builder } = webdriver;
 
@@ -7,23 +5,13 @@ const driver = new Builder()
     .forBrowser('chrome')
     .build();
 
-// open url
 driver.get('http://localhost:8000/index.html');
-
-// find element - the element is created by JavaScript so we have to wait
 driver.wait(until.elementLocated(By.id('cat_5')), 20 * 1000)
-.then(_ => driver.findElement(By.id('cat_3'))) // return found element
-.then(element => element.click())
-.then(_ => console.log('clicked on element'));
+.then(() => driver.findElement(By.id('cat_3')))   // return element
+.then(element => element.click())                 // click on element
+.then(() => console.log('clicked on element'));   // log to console
 
+// wait until the header shows
 driver.wait(until.elementLocated(By.id('success')), 20 * 1000)
-.then(_ => console.log('Success'))
-.then(_ => driver.quit());
-
-
-
-// driver.get('http://www.google.com/ncr');
-// driver.findElement(By.name('q')).sendKeys('webdriver');
-// driver.findElement(By.name('btnG')).click();
-// driver.wait(until.titleIs('webdriver - Google Search'), 1000);
-// driver.quit();
+.then(() => console.log('Success'))
+.then(() => driver.quit());
